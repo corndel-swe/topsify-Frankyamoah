b0400bb8a723bab8ff9d133f64e9eaa2c984b316
Frank Yamoah <frankyamoah94@gmail.com>
2025-10-07 16:31:29 +0100

    fixed issues

--- queries/chart-3.sql
+++ queries/chart-3.sql
@@ -1,2 +1,6 @@
 .mode json
-SELECT albums.name AS album_name, AV
+SELECT albums.name AS album_name, ROUND(AVG(features.loudness), 1) AS average_loudness, albums.release_date
+FROM albums
+INNER JOIN features, tracks, artists
+WHERE features.track_id = tracks.id AND tracks.album_id = albums.id AND artists.id = albums.artist_id
+GROUP BY album_id
